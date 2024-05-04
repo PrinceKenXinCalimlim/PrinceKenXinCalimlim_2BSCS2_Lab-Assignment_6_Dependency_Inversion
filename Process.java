3b65ee830044e877d44cf636c9f5ef4da317b5c5
Prince Ken Xin Calimlim <152839644+PrinceKenXinCalimlim@users.noreply.github.com>
2024-05-04 16:17:28 +0800

    Update Process.java

--- Process.java
+++ Process.java
@@ -1,13 +1,18 @@
 public class Process {
     private Student student;
     private BorrowBook book;
+    private BorrowJournal journal;
 
-    public Process(Student student, BorrowBook book) {
+    public Process(Student student, BorrowBook borrowBook,BorrowJournal borrowJournal) {
         this.student = student;
-        this.book = book;
+        this.book = borrowBook;
+        this.journal = borrowJournal;
     }
 
-    public void process() {
+    public void bookprocess() {
         book.borrow(student.getTitle());
     }
+    public void journalprocess(){
+    journal.borrow(student.getTitle());
+}
 }
