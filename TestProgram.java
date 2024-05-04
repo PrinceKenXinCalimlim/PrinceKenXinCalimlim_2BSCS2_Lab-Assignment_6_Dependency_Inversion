a30bc16d8dee4455f22150a03591aa8e02babe31
Prince Ken Xin Calimlim <152839644+PrinceKenXinCalimlim@users.noreply.github.com>
2024-05-04 16:48:58 +0800

    Update TestProgram.java

--- TestProgram.java
+++ TestProgram.java
@@ -1,20 +1,20 @@
 public class TestProgram {
     public static void main(String[] args) {
         
-            Student student1 = new Student("246", "Bateman", "Patrick", "American Psycho");
-            Process process1 = new Process(student1, new BorrowBook(), null);
-            process1.bookprocess();
+        Student student1 = new Student("246", "Bateman", "Patrick", "American Psycho");
+        Process process1 = new Process(student1, new BorrowBook());
+        process1.processBook();
     
-            Student student2 = new Student("456", "Corleone", "Michael", "The Godfather");
-            Process process2 = new Process(student2, new BorrowBook(), null);
-            process2.bookprocess();
+        Student student2 = new Student("456", "Corleone", "Michael", "The Godfather");
+        Process process2 = new Process(student2, new BorrowBook());
+        process2.processBook();
     
-            Student student3 = new Student("236", "Dunne", "Amy", "The Civil War Diary");
-            Process process3 = new Process(student3, null, new BorrowJournal());
-            process3.journalprocess();
+        Student student3 = new Student("236", "Dunne", "Amy", "The Civil War Diary");
+        JournalProcess process3 = new JournalProcess(student3, new BorrowJournal());
+        process3.processJournal();
     
-            Student student4 = new Student("556", "Thurman", "Uma", "The Journals of Sylvia Plath");
-            Process process4 = new Process(student4, null, new BorrowJournal());
-            process4.journalprocess();
-        }
+        Student student4 = new Student("556", "Thurman", "Uma", "The Journals of Sylvia Plath");
+        JournalProcess process4 = new JournalProcess(student4, new BorrowJournal());
+        process4.processJournal();
     }
+}
