221f65aee63ff32a71f7a04dfa76459e4174d782
Prince Ken Xin Calimlim <152839644+PrinceKenXinCalimlim@users.noreply.github.com>
2024-05-04 16:16:34 +0800

    Update TestProgram.java

--- TestProgram.java
+++ TestProgram.java
@@ -1,12 +1,20 @@
 public class TestProgram {
     public static void main(String[] args) {
         
-        Student student1 = new Student("246", "Bateman", "Patrick", "American Pyscho");
-        Process process1 = new Process(student1, new BorrowBook());
-        process1.process();
-
-        Student student2 = new Student("456", "Corleone", "Michael", "The GodFather");
-        Process process2 = new Process(student2, new BorrowBook());
-        process2.process();
+            Student student1 = new Student("246", "Bateman", "Patrick", "American Psycho");
+            Process process1 = new Process(student1, new BorrowBook(), null);
+            process1.bookprocess();
+    
+            Student student2 = new Student("456", "Corleone", "Michael", "The Godfather");
+            Process process2 = new Process(student2, new BorrowBook(), null);
+            process2.bookprocess();
+    
+            Student student3 = new Student("236", "Dunne", "Amy", "The Civil War Diary");
+            Process process3 = new Process(student3, null, new BorrowJournal());
+            process3.journalprocess();
+    
+            Student student4 = new Student("556", "Thurman", "Uma", "The Journals of Sylvia Plath");
+            Process process4 = new Process(student4, null, new BorrowJournal());
+            process4.journalprocess();
+        }
     }
-}
